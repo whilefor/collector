2be9380d080908755ecd5a045299eb02c31e5415
whilefor <469676693@qq.com>
2015-06-26 03:12:16 +0800

    update bower.json

--- collector.js
+++ collector.js
@@ -588,6 +588,570 @@
             'dashboardElement':dashboardElement
             };
     }
+
+    var client = function(){
+
+        //rendering engines
+        var engine = {            
+            ie: 0,
+            gecko: 0,
+            webkit: 0,
+            khtml: 0,
+            opera: 0,
+
+            //complete version
+            ver: null  
+        };
+        
+        //browsers
+        var browser = {
+            
+            //browsers
+            ie: 0,
+            firefox: 0,
+            safari: 0,
+            konq: 0,
+            opera: 0,
+            chrome: 0,
+            safari: 0,
+
+            //specific version
+            ver: null
+        };
+
+        
+        //platform/device/OS
+        var system = {
+            win: false,
+            mac: false,
+            x11: false,
+            
+            //mobile devices
+            iphone: false,
+            ipod: false,
+            ipad: false,
+            ios: false,
+            android: false,
+            nokiaN: false,
+            winMobile: false,
+            
+            //game systems
+            wii: false,
+            ps: false 
+        };    
+
+        //detect rendering engines/browsers
+        var ua = navigator.userAgent;    
+        if (window.opera){
+            engine.ver = browser.ver = window.opera.version();
+            engine.opera = browser.opera = parseFloat(engine.ver);
+        } else if (/AppleWebKit\/(\S+)/.test(ua)){
+            engine.ver = RegExp["$1"];
+            engine.webkit = parseFloat(engine.ver);
+            
+            //figure out if it's Chrome or Safari
+            if (/Chrome\/(\S+)/.test(ua)){
+                browser.ver = RegExp["$1"];
+                browser.chrome = parseFloat(browser.ver);
+            } else if (/Version\/(\S+)/.test(ua)){
+                browser.ver = RegExp["$1"];
+                browser.safari = parseFloat(browser.ver);
+            } else {
+                //approximate version
+                var safariVersion = 1;
+                if (engine.webkit < 100){
+                    safariVersion = 1;
+                } else if (engine.webkit < 312){
+                    safariVersion = 1.2;
+                } else if (engine.webkit < 412){
+                    safariVersion = 1.3;
+                } else {
+                    safariVersion = 2;
+                }   
+                
+                browser.safari = browser.ver = safariVersion;        
+            }
+        } else if (/KHTML\/(\S+)/.test(ua) || /Konqueror\/([^;]+)/.test(ua)){
+            engine.ver = browser.ver = RegExp["$1"];
+            engine.khtml = browser.konq = parseFloat(engine.ver);
+        } else if (/rv:([^\)]+)\) Gecko\/\d{8}/.test(ua)){    
+            engine.ver = RegExp["$1"];
+            engine.gecko = parseFloat(engine.ver);
+            
+            //determine if it's Firefox
+            if (/Firefox\/(\S+)/.test(ua)){
+                browser.ver = RegExp["$1"];
+                browser.firefox = parseFloat(browser.ver);
+            }
+        } else if (/MSIE ([^;]+)/.test(ua)){    
+            engine.ver = browser.ver = RegExp["$1"];
+            engine.ie = browser.ie = parseFloat(engine.ver);
+        }
+        
+        //detect browsers
+        browser.ie = engine.ie;
+        browser.opera = engine.opera;
+        
+
+        //detect platform
+        var p = navigator.platform;
+        system.win = p.indexOf("Win") == 0;
+        system.mac = p.indexOf("Mac") == 0;
+        system.x11 = (p == "X11") || (p.indexOf("Linux") == 0);
+
+        //detect windows operating systems
+        if (system.win){
+            if (/Win(?:dows )?([^do]{2})\s?(\d+\.\d+)?/.test(ua)){
+                if (RegExp["$1"] == "NT"){
+                    switch(RegExp["$2"]){
+                        case "5.0":
+                            system.win = "2000";
+                            break;
+                        case "5.1":
+                            system.win = "XP";
+                            break;
+                        case "6.0":
+                            system.win = "Vista";
+                            break;
+                        case "6.1":
+                            system.win = "7";
+                            break;
+                        default:
+                            system.win = "NT";
+                            break;                
+                    }                            
+                } else if (RegExp["$1"] == "9x"){
+                    system.win = "ME";
+                } else {
+                    system.win = RegExp["$1"];
+                }
+            }
+        }
+        
+        //mobile devices
+        system.iphone = ua.indexOf("iPhone") > -1;
+        system.ipod = ua.indexOf("iPod") > -1;
+        system.ipad = ua.indexOf("iPad") > -1;
+        system.nokiaN = ua.indexOf("NokiaN") > -1;
+        system.winMobile = (system.win == "CE");
+        
+        //determine iOS version
+        if (system.mac && ua.indexOf("Mobile") > -1){
+            if (/CPU (?:iPhone )?OS (\d+_\d+)/.test(ua)){
+                system.ios = parseFloat(RegExp.$1.replace("_", "."));
+            } else {
+                system.ios = 2;  //can't really detect - so guess
+            }
+        }
+        
+        //determine Android version
+        if (/Android (\d+\.\d+)/.test(ua)){
+            system.android = parseFloat(RegExp.$1);
+        }
+        
+        //gaming systems
+        system.wii = ua.indexOf("Wii") > -1;
+        system.ps = /playstation/i.test(ua);
+        
+        //return it
+        return {
+            engine:     engine,
+            browser:    browser,
+            system:     system        
+        };
+
+    }();
+
+    var EventUtil = {
+
+        addHandler: function(element, type, handler){
+            if (element.addEventListener){
+                element.addEventListener(type, handler, false);
+            } else if (element.attachEvent){
+                element.attachEvent("on" + type, handler);
+            } else {
+                element["on" + type] = handler;
+            }
+        },
+        
+        getButton: function(event){
+            if (document.implementation.hasFeature("MouseEvents", "2.0")){
+                return event.button;
+            } else {
+                switch(event.button){
+                    case 0:
+                    case 1:
+                    case 3:
+                    case 5:
+                    case 7:
+                        return 0;
+                    case 2:
+                    case 6:
+                        return 2;
+                    case 4: return 1;
+                }
+            }
+        },
+        
+        getCharCode: function(event){
+            if (typeof event.charCode == "number"){
+                return event.charCode;
+            } else {
+                return event.keyCode;
+            }
+        },
+        
+        getClipboardText: function(event){
+            var clipboardData =  (event.clipboardData || window.clipboardData);
+            return clipboardData.getData("text");
+        },
+        
+        getEvent: function(event){
+            return event ? event : window.event;
+        },
+        
+        getRelatedTarget: function(event){
+            if (event.relatedTarget){
+                return event.relatedTarget;
+            } else if (event.toElement){
+                return event.toElement;
+            } else if (event.fromElement){
+                return event.fromElement;
+            } else {
+                return null;
+            }
+        
+        },
+        
+        getTarget: function(event){
+            return event.target || event.srcElement;
+        },
+        
+        getWheelDelta: function(event){
+            if (event.wheelDelta){
+                return (client.engine.opera && client.engine.opera < 9.5 ? -event.wheelDelta : event.wheelDelta);
+            } else {
+                return -event.detail * 40;
+            }
+        },
+        
+        preventDefault: function(event){
+            if (event.preventDefault){
+                event.preventDefault();
+            } else {
+                event.returnValue = false;
+            }
+        },
+
+        removeHandler: function(element, type, handler){
+            if (element.removeEventListener){
+                element.removeEventListener(type, handler, false);
+            } else if (element.detachEvent){
+                element.detachEvent("on" + type, handler);
+            } else {
+                element["on" + type] = null;
+            }
+        },
+        
+        setClipboardText: function(event, value){
+            if (event.clipboardData){
+                event.clipboardData.setData("text/plain", value);
+            } else if (window.clipboardData){
+                window.clipboardData.setData("text", value);
+            }
+        },
+        
+        stopPropagation: function(event){
+            if (event.stopPropagation){
+                event.stopPropagation();
+            } else {
+                event.cancelBubble = true;
+            }
+        }
+
+    };
+
+    var client = function(){
+
+        //rendering engines
+        var engine = {            
+            ie: 0,
+            gecko: 0,
+            webkit: 0,
+            khtml: 0,
+            opera: 0,
+
+            //complete version
+            ver: null  
+        };
+        
+        //browsers
+        var browser = {
+            
+            //browsers
+            ie: 0,
+            firefox: 0,
+            safari: 0,
+            konq: 0,
+            opera: 0,
+            chrome: 0,
+            safari: 0,
+
+            //specific version
+            ver: null
+        };
+
+        
+        //platform/device/OS
+        var system = {
+            win: false,
+            mac: false,
+            x11: false,
+            
+            //mobile devices
+            iphone: false,
+            ipod: false,
+            ipad: false,
+            ios: false,
+            android: false,
+            nokiaN: false,
+            winMobile: false,
+            
+            //game systems
+            wii: false,
+            ps: false 
+        };    
+
+        //detect rendering engines/browsers
+        var ua = navigator.userAgent;    
+        if (window.opera){
+            engine.ver = browser.ver = window.opera.version();
+            engine.opera = browser.opera = parseFloat(engine.ver);
+        } else if (/AppleWebKit\/(\S+)/.test(ua)){
+            engine.ver = RegExp["$1"];
+            engine.webkit = parseFloat(engine.ver);
+            
+            //figure out if it's Chrome or Safari
+            if (/Chrome\/(\S+)/.test(ua)){
+                browser.ver = RegExp["$1"];
+                browser.chrome = parseFloat(browser.ver);
+            } else if (/Version\/(\S+)/.test(ua)){
+                browser.ver = RegExp["$1"];
+                browser.safari = parseFloat(browser.ver);
+            } else {
+                //approximate version
+                var safariVersion = 1;
+                if (engine.webkit < 100){
+                    safariVersion = 1;
+                } else if (engine.webkit < 312){
+                    safariVersion = 1.2;
+                } else if (engine.webkit < 412){
+                    safariVersion = 1.3;
+                } else {
+                    safariVersion = 2;
+                }   
+                
+                browser.safari = browser.ver = safariVersion;        
+            }
+        } else if (/KHTML\/(\S+)/.test(ua) || /Konqueror\/([^;]+)/.test(ua)){
+            engine.ver = browser.ver = RegExp["$1"];
+            engine.khtml = browser.konq = parseFloat(engine.ver);
+        } else if (/rv:([^\)]+)\) Gecko\/\d{8}/.test(ua)){    
+            engine.ver = RegExp["$1"];
+            engine.gecko = parseFloat(engine.ver);
+            
+            //determine if it's Firefox
+            if (/Firefox\/(\S+)/.test(ua)){
+                browser.ver = RegExp["$1"];
+                browser.firefox = parseFloat(browser.ver);
+            }
+        } else if (/MSIE ([^;]+)/.test(ua)){    
+            engine.ver = browser.ver = RegExp["$1"];
+            engine.ie = browser.ie = parseFloat(engine.ver);
+        }
+        
+        //detect browsers
+        browser.ie = engine.ie;
+        browser.opera = engine.opera;
+        
+
+        //detect platform
+        var p = navigator.platform;
+        system.win = p.indexOf("Win") == 0;
+        system.mac = p.indexOf("Mac") == 0;
+        system.x11 = (p == "X11") || (p.indexOf("Linux") == 0);
+
+        //detect windows operating systems
+        if (system.win){
+            if (/Win(?:dows )?([^do]{2})\s?(\d+\.\d+)?/.test(ua)){
+                if (RegExp["$1"] == "NT"){
+                    switch(RegExp["$2"]){
+                        case "5.0":
+                            system.win = "2000";
+                            break;
+                        case "5.1":
+                            system.win = "XP";
+                            break;
+                        case "6.0":
+                            system.win = "Vista";
+                            break;
+                        case "6.1":
+                            system.win = "7";
+                            break;
+                        default:
+                            system.win = "NT";
+                            break;                
+                    }                            
+                } else if (RegExp["$1"] == "9x"){
+                    system.win = "ME";
+                } else {
+                    system.win = RegExp["$1"];
+                }
+            }
+        }
+        
+        //mobile devices
+        system.iphone = ua.indexOf("iPhone") > -1;
+        system.ipod = ua.indexOf("iPod") > -1;
+        system.ipad = ua.indexOf("iPad") > -1;
+        system.nokiaN = ua.indexOf("NokiaN") > -1;
+        system.winMobile = (system.win == "CE");
+        
+        //determine iOS version
+        if (system.mac && ua.indexOf("Mobile") > -1){
+            if (/CPU (?:iPhone )?OS (\d+_\d+)/.test(ua)){
+                system.ios = parseFloat(RegExp.$1.replace("_", "."));
+            } else {
+                system.ios = 2;  //can't really detect - so guess
+            }
+        }
+        
+        //determine Android version
+        if (/Android (\d+\.\d+)/.test(ua)){
+            system.android = parseFloat(RegExp.$1);
+        }
+        
+        //gaming systems
+        system.wii = ua.indexOf("Wii") > -1;
+        system.ps = /playstation/i.test(ua);
+        
+        //return it
+        return {
+            engine:     engine,
+            browser:    browser,
+            system:     system        
+        };
+
+    }();
+
+    var EventUtil = {
+
+        addHandler: function(element, type, handler){
+            if (element.addEventListener){
+                element.addEventListener(type, handler, false);
+            } else if (element.attachEvent){
+                element.attachEvent("on" + type, handler);
+            } else {
+                element["on" + type] = handler;
+            }
+        },
+        
+        getButton: function(event){
+            if (document.implementation.hasFeature("MouseEvents", "2.0")){
+                return event.button;
+            } else {
+                switch(event.button){
+                    case 0:
+                    case 1:
+                    case 3:
+                    case 5:
+                    case 7:
+                        return 0;
+                    case 2:
+                    case 6:
+                        return 2;
+                    case 4: return 1;
+                }
+            }
+        },
+        
+        getCharCode: function(event){
+            if (typeof event.charCode == "number"){
+                return event.charCode;
+            } else {
+                return event.keyCode;
+            }
+        },
+        
+        getClipboardText: function(event){
+            var clipboardData =  (event.clipboardData || window.clipboardData);
+            return clipboardData.getData("text");
+        },
+        
+        getEvent: function(event){
+            return event ? event : window.event;
+        },
+        
+        getRelatedTarget: function(event){
+            if (event.relatedTarget){
+                return event.relatedTarget;
+            } else if (event.toElement){
+                return event.toElement;
+            } else if (event.fromElement){
+                return event.fromElement;
+            } else {
+                return null;
+            }
+        
+        },
+        
+        getTarget: function(event){
+            return event.target || event.srcElement;
+        },
+        
+        getWheelDelta: function(event){
+            if (event.wheelDelta){
+                return (client.engine.opera && client.engine.opera < 9.5 ? -event.wheelDelta : event.wheelDelta);
+            } else {
+                return -event.detail * 40;
+            }
+        },
+        
+        preventDefault: function(event){
+            if (event.preventDefault){
+                event.preventDefault();
+            } else {
+                event.returnValue = false;
+            }
+        },
+
+        removeHandler: function(element, type, handler){
+            if (element.removeEventListener){
+                element.removeEventListener(type, handler, false);
+            } else if (element.detachEvent){
+                element.detachEvent("on" + type, handler);
+            } else {
+                element["on" + type] = null;
+            }
+        },
+        
+        setClipboardText: function(event, value){
+            if (event.clipboardData){
+                event.clipboardData.setData("text/plain", value);
+            } else if (window.clipboardData){
+                window.clipboardData.setData("text", value);
+            }
+        },
+        
+        stopPropagation: function(event){
+            if (event.stopPropagation){
+                event.stopPropagation();
+            } else {
+                event.cancelBubble = true;
+            }
+        }
+
+    };
     window.collector = collector;
 
 })(window, document);
